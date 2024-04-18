6b0445ab779e2ed1803eb0450b04a2359a3da563
Mehmet Yasin Cifci <cifci@campus.tu-berlin.de>
2024-04-18 13:51:28 +0200

    cluster

--- template/run/run_pacs.sh
+++ template/run/run_pacs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=dispatch-bt
-#SBATCH --partition=gpu-2d
+#SBATCH --partition=gpu-2h
 #SBATCH --gpus-per-node=40gb:1
 #SBATCH --ntasks-per-node=4
 #SBATCH --output=logs/job-%j.out
